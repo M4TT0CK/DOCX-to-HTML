86ba2d90d62b083b73a8b1bff5aaee547fe88d48
James <JaimeHatchet@gmail.com>
2018-12-25 18:04:29 -0600

    v1.0.0

---
+++ DOCX-to-HTML/DOCX-to-HTML/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace DOCX_to_HTML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+            ofd.Filter = "DOCX files (*.docx)|*.docx|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.InputBox.Text = ofd.FileName;
+            }
+        }
+
+        private void Output_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".html";
+            dlg.Filter = "HTML files (.html)|*.html";
+            Nullable<bool> result = dlg.ShowDialog();
+            this.OutputBox.Text = dlg.FileName;
+        }
+
+        private void Conversion_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.InputBox.Text == "" || this.InputBox.Text == null)
+            {
+                MessageBoxResult result = MessageBox.Show("Please select an input DOCX file.", "ERROR");
+            }
+            else if(this.OutputBox.Text == "" || this.OutputBox.Text == null)
+            {
+                MessageBoxResult result = MessageBox.Show("Please select an output HTML file.", "ERROR");
+            }
+            else
+            {
+                Convert(InputBox.Text, OutputBox.Text);
+                MessageBoxResult result = MessageBox.Show("Conversion completed!", "Finished");
+            }
+        }
+
+        private void Convert(string inputFilePath, string outputFilePath)
+        {
+            var fileInfo = new FileInfo(inputFilePath);
+            string fullFilePath = fileInfo.FullName;
+            string htmlText = string.Empty;
+            try
+            {
+                htmlText = ParseDOCX(fileInfo);
+            }
+            catch (OpenXmlPackageException e)
+            {
+                if (e.ToString().Contains("Invalid Hyperlink"))
+                {
+                    using (FileStream fs = new FileStream(fullFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    {
+                        UriFixer.FixInvalidUri(fs, brokenUri => FixUri(brokenUri));
+                    }
+                    htmlText = ParseDOCX(fileInfo);
+                }
+            }
+
+            var writer = File.CreateText(outputFilePath);
+            writer.WriteLine(htmlText.ToString());
+            writer.Dispose();
+        }
+
+        public static Uri FixUri(string brokenUri)
+        {
+            string newURI = string.Empty;
+            if (brokenUri.Contains("mailto:"))
+            {
+                int mailToCount = "mailto:".Length;
+                brokenUri = brokenUri.Remove(0, mailToCount);
+                newURI = brokenUri;
+            }
+            else
+            {
+                newURI = " ";
+            }
+            return new Uri(newURI);
+        }
+
+        public static string ParseDOCX(FileInfo fileInfo)
+        {
+            try
+            {
+                byte[] byteArray = File.ReadAllBytes(fileInfo.FullName);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(byteArray, 0, byteArray.Length);
+                    using (WordprocessingDocument wDoc =
+                                                WordprocessingDocument.Open(memoryStream, true))
+                    {
+                        int imageCounter = 0;
+                        var pageTitle = fileInfo.FullName;
+                        var part = wDoc.CoreFilePropertiesPart;
+                        if (part != null)
+                            pageTitle = (string)part.GetXDocument()
+                                                    .Descendants(DC.title)
+                                                    .FirstOrDefault() ?? fileInfo.FullName;
+
+                        WmlToHtmlConverterSettings settings = new WmlToHtmlConverterSettings()
+                        {
+                            AdditionalCss = "body { margin: 1cm auto; max-width: 20cm; padding: 0; }",
+                            PageTitle = pageTitle,
+                            FabricateCssClasses = true,
+                            CssClassPrefix = "pt-",
+                            RestrictToSupportedLanguages = false,
+                            RestrictToSupportedNumberingFormats = false,
+                            ImageHandler = imageInfo =>
+                            {
+                                ++imageCounter;
+                                string extension = imageInfo.ContentType.Split('/')[1].ToLower();
+                                ImageFormat imageFormat = null;
+                                if (extension == "png") imageFormat = ImageFormat.Png;
+                                else if (extension == "gif") imageFormat = ImageFormat.Gif;
+                                else if (extension == "bmp") imageFormat = ImageFormat.Bmp;
+                                else if (extension == "jpeg") imageFormat = ImageFormat.Jpeg;
+                                else if (extension == "tiff")
+                                {
+                                    extension = "gif";
+                                    imageFormat = ImageFormat.Gif;
+                                }
+                                else if (extension == "x-wmf")
+                                {
+                                    extension = "wmf";
+                                    imageFormat = ImageFormat.Wmf;
+                                }
+
+                                if (imageFormat == null) return null;
+
+                                string base64 = null;
+                                try
+                                {
+                                    using (MemoryStream ms = new MemoryStream())
+                                    {
+                                        imageInfo.Bitmap.Save(ms, imageFormat);
+                                        var ba = ms.ToArray();
+                                        base64 = System.Convert.ToBase64String(ba);
+                                    }
+                                }
+                                catch (System.Runtime.InteropServices.ExternalException)
+                                { return null; }
+
+                                ImageFormat format = imageInfo.Bitmap.RawFormat;
+                                ImageCodecInfo codec = ImageCodecInfo.GetImageDecoders()
+                                                            .First(c => c.FormatID == format.Guid);
+                                string mimeType = codec.MimeType;
+
+                                string imageSource =
+                                        string.Format("data:{0};base64,{1}", mimeType, base64);
+
+                                XElement img = new XElement(Xhtml.img,
+                                        new XAttribute(NoNamespace.src, imageSource),
+                                        imageInfo.ImgStyleAttribute,
+                                        imageInfo.AltText != null ?
+                                            new XAttribute(NoNamespace.alt, imageInfo.AltText) : null);
+                                return img;
+                            }
+                        };
+
+                        XElement htmlElement = WmlToHtmlConverter.ConvertToHtml(wDoc, settings);
+                        var html = new XDocument(new XDocumentType("html", null, null, null),
+                                                                                    htmlElement);
+                        var htmlString = html.ToString(SaveOptions.DisableFormatting);
+                        return htmlString;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBoxResult result = MessageBox.Show("The file entered is either open (please close it) or contains corrupted data.", "ERROR");
+                return "The file entered is either open (please close it) or contains corrupted data.";
+            }
+        }
+    }
+}
